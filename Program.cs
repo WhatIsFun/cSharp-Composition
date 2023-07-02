c34b123773953539208426665ad99e18d250d858
WhatIsFun <mohammed110051@gmail.com>
2023-07-02 12:34:19 +0400

    Composition Example

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace cSharp_Composition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
